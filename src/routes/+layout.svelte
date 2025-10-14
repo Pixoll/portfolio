d1e6af97dd4326d005157773af7c4d5007fae7de
Pixoll <pixoll.gamer@gmail.com>
2025-10-14 12:39:46 -0300

    refactor: move common structure to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import "../app.css";
   import favicon from "$lib/assets/favicon.svg";
+  import Header from "$lib/components/sections/Header.svelte";
   import { t } from "$lib/i18n/store";
   import type { Snippet } from "svelte";
 
@@ -16,4 +17,24 @@
   <title>{$t.mainPageTitle}</title>
 </svelte:head>
 
-{@render children?.()}
+<div class="fixed left-0 -z-10 w-full h-screen overflow-clip">
+  <div class="relative -top-[575vh] dark:top-0 [transition:top_1500ms]">
+    <div class="h-[125vh] bg-gradient-to-t from-background3 via-background2 to-background1"></div>
+    <div class="h-[175vh] bg-gradient-to-t from-background4 to-background3"></div>
+    <div class="h-[100vh] bg-gradient-to-t from-background6 via-background5 to-background4"></div>
+    <div class="h-[150vh] bg-gradient-to-t from-background8 via-background7 to-background6"></div>
+    <div class="h-[50vh] bg-background8"></div>
+    <div class="h-[75vh] bg-gradient-to-t from-background9 to-background8"></div>
+  </div>
+</div>
+
+<Header/>
+<main
+  class={
+    "max-w-[1250px] mx-auto px-12 pt-24 pb-8 text-text max-sm:px-4"
+    + " [transition:color_1500ms,background-color_1500ms,padding_200ms]"
+  }
+  lang={$t.langId}
+>
+  {@render children?.()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,36 +2,14 @@
   import AboutMe from "$lib/components/sections/AboutMe.svelte";
   import Education from "$lib/components/sections/Education.svelte";
   import Experience from "$lib/components/sections/Experience.svelte";
-  import Header from "$lib/components/sections/Header.svelte";
   import Home from "$lib/components/sections/Home.svelte";
   import Projects from "$lib/components/sections/Projects.svelte";
-  import { t } from "$lib/i18n/store";
 </script>
 
-<div class="fixed left-0 -z-10 w-full h-screen overflow-clip">
-  <div class="relative -top-[575vh] dark:top-0 [transition:top_1500ms]">
-    <div class="h-[125vh] bg-gradient-to-t from-background3 via-background2 to-background1"></div>
-    <div class="h-[175vh] bg-gradient-to-t from-background4 to-background3"></div>
-    <div class="h-[100vh] bg-gradient-to-t from-background6 via-background5 to-background4"></div>
-    <div class="h-[150vh] bg-gradient-to-t from-background8 via-background7 to-background6"></div>
-    <div class="h-[50vh] bg-background8"></div>
-    <div class="h-[75vh] bg-gradient-to-t from-background9 to-background8"></div>
-  </div>
+<div class="space-y-4">
+  <Home/>
+  <AboutMe/>
+  <Experience/>
+  <Projects/>
+  <Education/>
 </div>
-
-<Header/>
-<main
-  class={
-    "min-h-screen px-12 pt-24 pb-8 text-text max-sm:px-4"
-    + " [transition:color_1500ms,background-color_1500ms,padding_200ms]"
-  }
-  lang={$t.langId}
->
-  <div class="flex flex-col gap-4 max-w-[1250px] mx-auto">
-    <Home/>
-    <AboutMe/>
-    <Experience/>
-    <Projects/>
-    <Education/>
-  </div>
-</main>
